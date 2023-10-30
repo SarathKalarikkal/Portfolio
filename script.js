e9554156766f700195cc293fda0beade0811778d
Sarath Kalarikkal <sarathkalarikkal0@gmail.com>
2023-10-30 13:18:06 +0530

    Updated Profession animation and added svg

--- script.js
+++ script.js
@@ -1,84 +1,63 @@
-// Paragrah words hover effects in the about section
-const paragraphs = document.querySelectorAll('#about p');
+// -----------Profesion Typing effect on hero section-----------  
 
-paragraphs.forEach((paragraph) => {
-    const words = paragraph.textContent.split(' ');
-    paragraph.innerHTML = '';
-
-    words.forEach((word) => {
-        const span = document.createElement('span');
-        span.textContent = word + ' ';
-        span.addEventListener('mouseenter', () => {
-            span.style.color = 'var(--brand-color)';
-        });
-        span.addEventListener('mouseleave', () => {
-            span.style.color = ''; // Reset the color on mouse leave
-        });
-        paragraph.appendChild(span);
-    });
-});
-
-// Profession Typing effect in the hero section
-class TxtRotate {
-    constructor(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-
-    tick() {
-        const i = this.loopNum % this.toRotate.length;
-        const fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-        const that = this;
-        const delta = 300 - Math.random() * 100;
-
-        if (this.isDeleting) {
-            delta /= 2;
-        }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-            delta = this.period;
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            this.loopNum++;
-            delta = 500;
-        }
-
-        setTimeout(function () {
-            that.tick();
-        }, delta);
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
     }
-}
-
-window.onload = function () {
-    const elements = document.querySelectorAll('.txt-rotate');
-    for (let i = 0; i < elements.length; i++) {
-        const toRotate = elements[i].getAttribute('data-rotate');
-        const period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
-        }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
     }
     // INJECT CSS
-    const css = document.createElement("style");
+    var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
-};
+  };
+ // -------------------------------------------------------------------  
+
 
 // Toggle menu on the navigation bar for mobile devices
 const menuItems = document.querySelector(".nav-item ul");
