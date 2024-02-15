7a7f4de86cba52001f1fc1e46811256dc5b419c1
Sarath Kalarikkal <sarathkalarikkal0@gmail.com>
2024-02-15 14:25:15 +0530

    updated HTML, CSS , JS,  set the contact form and new js projects

--- script.js
+++ script.js
@@ -259,11 +259,41 @@ switch (questionId) {
 
 
 
-
-
-
-
-
-
 // Axios Scrolling effect on every section
 AOS.init();
+
+
+
+let form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  let status = document.getElementById("my-form-status");
+  let data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thank you for your submission! I will reach out to you soon.";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
+
+
+
